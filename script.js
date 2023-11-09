959164cb6a7e98f5e8d649781a01d8d51496abbc
m0h4mm3d-0 <58182483+m4hunt3r@users.noreply.github.com>
2023-11-09 16:49:22 +0100

    created game mechanism/function

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function getComputerChoice() {
+	let choices = ["Rock", "Paper", "Scissors"];
+	let choice = choices[Math.floor(Math.random()*3)];
+	return choice;
+}
+
+function playRound(playerSelection, computerSelection) {
+	if (playerSelection === "rock" && computerSelection === "paper") {
+		return "you Lose! Paper beats Rock";
+	} else if (playerSelection === "rock" && computerSelection === "scissors") {
+		return "you Win! Rock beats Scissors";
+	} else if (playerSelection === "paper" && computerSelection === "rock") {
+		return "you Win! Paper beats Rock";
+	} else if (playerSelection === "paper" && computerSelection === "scissors") {
+		return "you Lose! Scissors beats Paper";
+	} else if (playerSelection === "scissors" && computerSelection === "rock") {
+		return "you Lose! Rock beats Scissors";
+	} else if (playerSelection === "scissors" && computerSelection === "paper") {
+		return "you Win! Scissor beats Paper";
+	} else return "Draw!";
+}
+
+const playerSelection = prompt("make a choice: ").toLowerCase();
+const computerSelection = getComputerChoice().toLowerCase();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
