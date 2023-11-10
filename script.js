b81a03d0dd8eb0c38138e5297671270e31cffb5f
m0h4mm3d-0 <58182483+m4hunt3r@users.noreply.github.com>
2023-11-10 20:43:19 +0100

    removed some stuff

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
-
+let computerScore = 0;
+let playerScore = 0;
 function getComputerChoice() {
 	let choices = ["Rock", "Paper", "Scissors"];
 	let choice = choices[Math.floor(Math.random()*3)];
@@ -8,21 +8,29 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
 	if (playerSelection === "rock" && computerSelection === "paper") {
+		computerScore++;
 		return "you Lose! Paper beats Rock";
 	} else if (playerSelection === "rock" && computerSelection === "scissors") {
+		playerScore++;
 		return "you Win! Rock beats Scissors";
 	} else if (playerSelection === "paper" && computerSelection === "rock") {
+		playerScore++;
 		return "you Win! Paper beats Rock";
 	} else if (playerSelection === "paper" && computerSelection === "scissors") {
+		computerScore++;
 		return "you Lose! Scissors beats Paper";
 	} else if (playerSelection === "scissors" && computerSelection === "rock") {
+		computerScore++;
 		return "you Lose! Rock beats Scissors";
 	} else if (playerSelection === "scissors" && computerSelection === "paper") {
+		playerScore++;
 		return "you Win! Scissor beats Paper";
-	} else if ((playerSelection === "rock" && computerSelection === "rock") || (playerSelection === "paper" && computerSelection === "paper") || (playerSelection === "scissors" && computerSelection === "scissors")) {
+	} else if (playerSelection === computerSelection) {
 		return "Draw!"
 	} else return "errorrrr!"
+	
 }
+
 
 function game() {
 	for (let i = 0; i <= 5; i++) {
@@ -30,7 +38,7 @@ function game() {
 		const computerSelection = getComputerChoice().toLowerCase();
 		console.log(computerSelection);
 		console.log(playRound(playerSelection, computerSelection));
-		
+
 	}
 }
 
