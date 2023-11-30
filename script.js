d2c44ee091c03b650dc30fb268fa416e1ab6a6b3
m0h4mm3d-0 <58182483+m4hunt3r@users.noreply.github.com>
2023-11-30 10:23:24 +0100

    game logic working just fine, and outputing results to page with DOM elements is succesfull so far, have a little bug to fix though

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let computerScore = 0;
 let playerScore = 0;
+const rockbtn = document.querySelector('.rock');
+const paperbtn = document.querySelector('.paper');
+const scissorsbtn = document.querySelector('.scissors');
+const resultDiv = document.querySelector('.result');
+
+
 function getComputerChoice() {
 	let choices = ["Rock", "Paper", "Scissors"];
 	let choice = choices[Math.floor(Math.random()*3)];
@@ -7,30 +13,61 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-	let log = ""; 
+	const p = document.createElement('p'); 
 	if (playerSelection === computerSelection) {
-		log = "Tie!";
+		p.innerText = "Tie!";
 	} else if (playerSelection === "rock" && computerSelection === "scissors" ||
 		playerSelection === "paper" && computerSelection === "rock" ||
 		playerSelection === "scissors" && computerSelection === "paper") {
 		playerScore++;
-		log = (`you Win! ${playerSelection} beats ${computerSelection}`);
+		p.innerText = (`you Win! ${playerSelection} beats ${computerSelection}`);
 	} else {
 		computerScore++;
-		log = (`you Lose! ${computerSelection} beats ${playerSelection}`);	
+		p.innerText = (`you Lose! ${computerSelection} beats ${playerSelection}`);	
 	}
-	return log;
+	return resultDiv.appendChild(p);
 }
 
-
-function game() {
-	for (let i = 0; i <= 5; i++) {
-		const playerSelection = prompt("Rock, Paper, Scissors?: ").toLowerCase();
-		const computerSelection = getComputerChoice().toLowerCase();
-		console.log(computerSelection);
-		console.log(playRound(playerSelection, computerSelection));
-		console.log(playerScore, "-", computerScore);
+const winnerCheck = (playerScore, computerScore) => {
+	const h2 = document.createElement('h2');
+	if (playerScore === 5) {
+		h2.innerText = `you won, ${playerScore} - ${computerScore}`;
+	} else if (computerScore === 5) {
+		h2.innerText = `you lost, ${playerScore} - ${computerScore}`;
 	}
+	return resultDiv.appendChild(h2);
 }
 
-game();
+rockbtn.addEventListener('click', () => {
+	const computerSelection = getComputerChoice().toLowerCase();
+	const playerSelection = 'rock';
+	playRound(playerSelection, computerSelection);
+	winnerCheck(playerScore, computerScore);
+})
+
+paperbtn.addEventListener('click', () => {
+	const computerSelection = getComputerChoice().toLowerCase();
+	const playerSelection = 'paper';
+	playRound(playerSelection, computerSelection);
+	winnerCheck(playerScore, computerScore);
+})
+
+scissorsbtn.addEventListener('click', () => {
+	const computerSelection = getComputerChoice().toLowerCase();
+	const playerSelection = 'scissors';
+	playRound(playerSelection, computerSelection);
+	winnerCheck(playerScore, computerScore);
+})
+
+
+//function game() {
+//	for (let i = 0; i <= 5; i++) {
+//		const playerSelection = prompt("Rock, Paper, Scissors?: ").toLowerCase();
+//		const computerSelection = getComputerChoice().toLowerCase();
+//		console.log(computerSelection);
+//		console.log(playRound(playerSelection, computerSelection));
+//		console.log(playerScore, "-", computerScore);
+//	}
+//}
+//
+//game();
